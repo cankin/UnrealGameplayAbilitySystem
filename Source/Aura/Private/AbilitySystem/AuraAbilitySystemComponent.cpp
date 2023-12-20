26af68fce2d60c4b63ca3dea26d4fd91a5530d21
LoreStudio-Camkin <christianmccown@gmail.com>
2023-12-20 11:29:23 -0600

    added a loop to print gameplay tags on gameplay Effects

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,7 +11,14 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
 	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
-	
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for(const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Turquoise, Msg);
+	}
 }
 
 
