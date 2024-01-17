1b5b3b78fa85ca7b8c649a0ef9ed2cfe6cb9a0e0
LoreStudio-Camkin <christianmccown@gmail.com>
2024-01-17 11:00:38 -0600

    Added all attributes to the AuraGameplayTags class

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -17,7 +17,21 @@ public:
  static const FAuraGameplayTags& Get() { return GameplayTags;}
  static void InitializeNativeGameplayTags();
 
+ FGameplayTag Attributes_Primary_Strength;
+ FGameplayTag Attributes_Primary_Intelligence;
+ FGameplayTag Attributes_Primary_Resilience;
+ FGameplayTag Attributes_Primary_Vigor;
+
  FGameplayTag Attributes_Secondary_Armor;
+ FGameplayTag Attributes_Secondary_ArmorPenetration;
+ FGameplayTag Attributes_Secondary_BlockChance;
+ FGameplayTag Attributes_Secondary_CritHitChance;
+ FGameplayTag Attributes_Secondary_CritHitDamage;
+ FGameplayTag Attributes_Secondary_CritHitResistance;
+ FGameplayTag Attributes_Secondary_HealthRegen;
+ FGameplayTag Attributes_Secondary_ManaRegen;
+ FGameplayTag Attributes_Secondary_MaxHealth;
+ FGameplayTag Attributes_Secondary_MaxMana;
 protected:
 private:
  static FAuraGameplayTags GameplayTags;
