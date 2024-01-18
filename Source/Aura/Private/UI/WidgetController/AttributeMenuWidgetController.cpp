80391bf1b8b7d5160caea01de8219d12692fa648
LoreStudio-Camkin <christianmccown@gmail.com>
2024-01-18 15:04:50 -0600

    Added the logic to find the correct attribute for each row

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,6 +2,9 @@
 
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
@@ -9,4 +12,11 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AuraAttributeSet =  CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo)
+
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AuraAttributeSet->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
